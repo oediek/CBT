e4caeb72b06cf87f9b3354a4c9910afd5d12e43e
Akhmad Zaini <zaini1983@gmail.com>
2018-10-07 19:53:24 +0700

    Penambahan fasilitas edit profil proktor

--- application/controllers/proktor/Profil.php
+++ application/controllers/proktor/Profil.php
@@ -6,7 +6,24 @@ require_once APPPATH . 'controllers/proktor/Home_proktor.php';
 
 class Profil extends Home_proktor{
 	function edit(){
-		$this->load->view('proktor/edit_profil.php');
+		$this->db->where('login', $this->session->login);
+		$data['profil'] = $this->db->get('proktor')->row();
+		$this->load->view('proktor/edit_profil.php', $data);
+	}
+
+	function submit_edit(){
+		$post = $this->input->post();
+		$secret = $this->config->item('encryption_key');
+		$this->db->set('nama', $post['nama']);
+		$this->db->set('email', $post['email']);
+		if(!empty($post['password'])){
+			$this->db->set('password', "AES_ENCRYPT('$post[password]', '$secret'", FALSE);
+		}
+		$this->db->where('login',  $this->session->login);
+		$this->db->update('proktor');
+		$this->session->pesan = 'sukses';
+		$this->session->mark_as_flash('pesan');
+		redirect('?d=proktor&c=profil&m=edit');
 	}
 
 }
--- theme/adminbsb/proktor/edit_profil.php
+++ theme/adminbsb/proktor/edit_profil.php
@@ -10,6 +10,12 @@
                 </h2>
             </div>
 
+            <?php if($this->session->pesan == 'sukses'):?>
+            <div class="alert alert-success">
+                Data profil telah tersimpan.
+            </div>
+            <?php endif?>
+
             <div class="row clearfix">
                 <div class="col-lg-12">
                     <div class="card">
@@ -20,34 +26,34 @@
                             </h2>
                         </div>
                         <div class="body">
-							<form action="" method="post">
+							<form action="?d=proktor&c=profil&m=submit_edit" method="post" id="frm-editor-profil">
                                 <label>Nama Lengkap</label>
                                 <div class="form-group">
                                     <div class="form-line">
-                                        <input type="text" class="form-control" name="nama" value="" placeholder="Nama lengkap" required="">
+                                        <input type="text" class="form-control" name="nama" value="<?=$profil->nama?>" placeholder="Nama lengkap" required="">
                                     </div>
                                 </div>
                                 <label>Alamat Surel</label>
                                 <div class="form-group">
                                     <div class="form-line">
-                                        <input type="email" class="form-control" name="email" value="" placeholder="Alamat surat elektronik">
+                                        <input type="email" class="form-control" name="email" value="<?=$profil->email?>" placeholder="Alamat surat elektronik">
                                     </div>
                                 </div>
                                 <label>Password</label>
                                 <div class="form-group">
                                     <div class="form-line">
-                                        <input type="password" class="form-control" name="email" value="">
+                                        <input type="password" class="form-control" name="password" value="">
                                     </div>
                                     <small class="help-block">Biarkan kosong jika tak ingin mengganti password</small>
                                 </div>
                                 <label>Konfirmasi Password</label>
                                 <div class="form-group">
                                     <div class="form-line">
-                                        <input type="password2" class="form-control" name="email" value="">
+                                        <input type="password2" class="form-control" name="password2" value="">
                                     </div>
                                     <small class="help-block">Ketik ulang password</small>
                                 </div>
-                                <button type="submit" class="btn btn-primary waves-effect">SIMPAN</button>  
+                                <button type="button" class="btn btn-primary waves-effect btn-simpan">SIMPAN</button>  
                             </form>
                         </div>
                     </div>
@@ -57,3 +63,32 @@
     </section>
 
 <?php $this->load->view('proktor/footer')?>
+
+<script>
+    $('.btn-simpan').on('click', function(evt){
+        var p1 = $('[name="password"]').val();
+        var p2 = $('[name="password2"]').val();
+        if(p1 != p2){
+            swal({
+                title: "Password",
+                text: "Password dan konfirmasi password tidak sama"
+            });
+            return;
+        }
+        swal({
+            title: "Anda yakin ?",
+            text: "Anda akan mengubah profil diri anda ?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Ya, Saya yakin",
+            cancelButtonText: "Tidak",
+            closeOnConfirm: true
+        },function(isEdited){
+            if(isEdited){
+                $('#frm-editor-profil').trigger('submit');
+            }
+        });        
+    });
+
+</script>
