079f9c8d0cf66e425630ce7b98473c04fbfd698c
Akhmad Zaini <zaini1983@gmail.com>
2018-11-14 14:17:47 +0700

    Revisi upload soal
    Perbaikan upload soal untuk kasus soal tabel bersarang

--- application/controllers/Word.php
+++ application/controllers/Word.php
@@ -43,9 +43,9 @@ class Word extends CI_Controller {
 			return $this->xmlrpc->send_error_message('100', 'Invalid Access');
 		}
     
-		$lembar_soal = $konten['description'];
+    $lembar_soal = $konten['description'];
     
-		$ujian = $this->__parse_soal($lembar_soal);
+    $ujian = $this->__parse_soal($lembar_soal);
     
 		if($this->__lolos_verifikasi_ujian($ujian)){
       $ujian['judul'] = trim($konten['title']);
@@ -133,9 +133,10 @@ class Word extends CI_Controller {
 			return FALSE;
 		}
     
-		// Periksa apakah jumlah soal telah sesuai dengan setting(konfigurasi) di Excel
-		if($r['jml_soal'] != count($ujian['soal'])){
-			log_message('custom', 'Jumlah soal di ms word tidak sama dengan Excel, IP : ' . $this->ip);
+    // Periksa apakah jumlah soal telah sesuai dengan setting(konfigurasi) di Excel
+    $jml_soal_word = count($ujian['soal']);
+		if($r['jml_soal'] != $jml_soal_word){
+			log_message('custom', "Jumlah soal di ms word ($jml_soal_word) tidak sama dengan Excel ($r[jml_soal]), IP : " . $this->ip);
 			return FALSE;
 		}
     
@@ -155,83 +156,112 @@ class Word extends CI_Controller {
       $ujian_id = $dom->getElementsByTagName('td')->item(1)->nodeValue;
       
       // Mengekstrak body soal
-      $dom_soal = $dom->getElementsByTagName('table')->item(0)->getElementsByTagName('tr');
+      $dom_soal = $dom->getElementsByTagName('tbody')->item(0)->childNodes;
+      
       $arr_soal = array();
       $nomor = 1;
-      for($n = 1; $n < $dom_soal->length; $n++){
-        $kolom = $dom_soal->item($n)->getElementsByTagName('td');
-        
+      
+      foreach($dom_soal as $k => $baris){
+        if($k == 0) {continue;}
+        $kolom = $baris->childNodes;
         if($kolom->length == 4){
           // Jika baris merupakan baris soal
           $soal = array(	
-            'konten' 	=> get_inner_html($kolom->item(1)), 
+            'konten' 	=> $this->__sanitize_from_word(get_inner_html($kolom->item(1))), 
             'jawaban' => trim(filter_var($kolom->item(2)->nodeValue, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH), " "),
             'skor' 		=> filter_var($kolom->item(3)->nodeValue, FILTER_SANITIZE_NUMBER_FLOAT),
             'essay'		=> 0
           );
-          $arr_soal[$nomor++] = array('soal' 		=> $soal,
-          'pilihan' 	=> array());
+          $arr_soal[$nomor++] = array(
+            'soal' 		=> $soal,
+            'pilihan' 	=> array()
+          );
         }else if($kolom->length == 2){
           // Jika baris merupakan baris soal essay
-          $soal = array(	'konten' 	=> get_inner_html($kolom->item(1)),
-          'jawaban'	=> '',
-          'skor'		=> 0,     							
-          'essay'		=> 1
-        );
-        $arr_soal[$nomor++] = array('soal' 		=> $soal,
-        'pilihan' 	=> array());
+          $soal = array(	
+            'konten' 	=> get_inner_html($kolom->item(1)),
+            'jawaban'	=> '',
+            'skor'		=> 0,     							
+            'essay'		=> 1
+          );
+          $arr_soal[$nomor++] = array(
+            'soal' 		=> $soal,
+            'pilihan' 	=> array()
+          );
+        }else if($kolom->length == 5){
+          // Jika baris merupakan baris jawaban
+          $idx = trim(filter_var($kolom->item(1)->nodeValue, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH), " ");
+          $jawab = get_inner_html($kolom->item(2));
+          $arr_soal[count($arr_soal)]['pilihan'][$idx] = $jawab;
+        }
       }
-      else{
-        // Jika baris merupakan baris jawaban
-        $idx = trim(filter_var($kolom->item(1)->nodeValue, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH), " ");
-        $jawab = get_inner_html($kolom->item(2));
-        $arr_soal[count($arr_soal)]['pilihan'][$idx] = $jawab;
+      
+      return array(	'ujian_id'	=> $ujian_id,
+      'soal'		=> $arr_soal,
+    );
+  }
+  
+  private function __get_ujian($ujian_id){
+    $q = $this->db->query("SELECT * FROM ujian WHERE ujian_id = '$ujian_id'");
+    return $q->row_array();
+  }
+  
+  private function __update_db_soal($lembar_soal, $ujian){
+    $ujian_id = $ujian['ujian_id'];
+    $arr_soal = $ujian['soal'];
+    
+    // Update data ujian
+    $sql = "UPDATE ujian 
+    SET judul = '$ujian[judul]', konten = '$lembar_soal', status_soal = 1, tgl_unggah = NOW()
+    WHERE ujian_id = '$ujian_id' ";
+    $this->db->query($sql);
+    
+    // Reset pilihan soal
+    $sql = "DELETE FROM pilihan_jawaban WHERE ujian_id = '$ujian_id'";
+    $this->db->query($sql);
+    
+    // Reset soal
+    $sql = "DELETE FROM soal WHERE ujian_id = '$ujian_id'";
+    $this->db->query($sql);
+    
+    // Simpan soal beserta pilihan jawaban
+    $sql = "INSERT INTO soal (ujian_id, no_soal, essay, konten, jawaban, skor) VALUES ";
+    $sql2 = "INSERT INTO pilihan_jawaban (ujian_id, no_soal, pilihan_ke, konten) VALUES ";
+    $baris = array();
+    $baris2 = array();
+    foreach($arr_soal as $no_soal => $butir){
+      $soal = $butir['soal'];
+      $baris[] = "('$ujian_id', $no_soal, '$soal[essay]', '$soal[konten]', '$soal[jawaban]', $soal[skor])";
+      foreach($butir['pilihan'] as $pilihan_ke => $konten){
+        $baris2[] = "('$ujian_id', $no_soal, '$pilihan_ke', '$konten')";
       }
+    }
+    $sql .= implode(',', $baris);
+    $sql2 .= implode(',', $baris2);
+    $this->db->query($sql);
+    $this->db->query($sql2);
+  }
+  
+  private function __sanitize_from_word( $content ){
+    // Convert microsoft special characters
+    $replace = array(
+      "‘" => "'",
+      "’" => "'",
+      "”" => '"',
+      "“" => '"',
+      "–" => "-",
+      "—" => "-",
+      "…" => "&#8230;"
+    );
+    
+    foreach($replace as $k => $v)
+    {
+      $content = str_replace($k, $v, $content);
     }
     
-    return array(	'ujian_id'	=> $ujian_id,
-    'soal'		=> $arr_soal,
-  );
-}
-
-private function __get_ujian($ujian_id){
-  $q = $this->db->query("SELECT * FROM ujian WHERE ujian_id = '$ujian_id'");
-  return $q->row_array();
-}
-
-private function __update_db_soal($lembar_soal, $ujian){
-  $ujian_id = $ujian['ujian_id'];
-  $arr_soal = $ujian['soal'];
-  
-  // Update data ujian
-  $sql = "UPDATE ujian 
-  SET judul = '$ujian[judul]', konten = '$lembar_soal', status_soal = 1, tgl_unggah = NOW()
-  WHERE ujian_id = '$ujian_id' ";
-  $this->db->query($sql);
-  
-  // Reset pilihan soal
-  $sql = "DELETE FROM pilihan_jawaban WHERE ujian_id = '$ujian_id'";
-  $this->db->query($sql);
-  
-  // Reset soal
-  $sql = "DELETE FROM soal WHERE ujian_id = '$ujian_id'";
-  $this->db->query($sql);
-  
-  // Simpan soal beserta pilihan jawaban
-  $sql = "INSERT INTO soal (ujian_id, no_soal, essay, konten, jawaban, skor) VALUES ";
-  $sql2 = "INSERT INTO pilihan_jawaban (ujian_id, no_soal, pilihan_ke, konten) VALUES ";
-  $baris = array();
-  $baris2 = array();
-  foreach($arr_soal as $no_soal => $butir){
-    $soal = $butir['soal'];
-    $baris[] = "('$ujian_id', $no_soal, '$soal[essay]', '$soal[konten]', '$soal[jawaban]', $soal[skor])";
-    foreach($butir['pilihan'] as $pilihan_ke => $konten){
-      $baris2[] = "('$ujian_id', $no_soal, '$pilihan_ke', '$konten')";
-    }
+    // Remove any non-ascii character
+    $content = preg_replace('/[^\x20-\x7E]*/','', $content);
+    
+    return $content;
   }
-  $sql .= implode(',', $baris);
-  $sql2 .= implode(',', $baris2);
-  $this->db->query($sql);
-  $this->db->query($sql2);
-}
 }
