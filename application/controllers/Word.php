ef5ed534534a67ccefb5468da9ed44f5c1cf1b4c
Akhmad Zaini <zaini1983@gmail.com>
2018-11-16 22:28:02 +0700

    Hack pilihan
    Untuk sementara pilihan hanya menerima pilihan A sd D

--- application/controllers/Word.php
+++ application/controllers/Word.php
@@ -224,7 +224,12 @@ class Word extends CI_Controller {
         );
       }else if($kolom->length == 5){
         // Jika baris merupakan baris jawaban
-        $idx = trim($kolom->item(1)->nodeValue, " ");
+        $idx = strtoupper(trim($kolom->item(1)->nodeValue, " "));
+
+        // hack pilihan untuk keperluan sessat (hanya menampung pilihan A s/d D)
+        $hack_pilihan = ['A', 'B', 'C', 'D'];
+        if(!in_array($idx, $hack_pilihan)) continue;
+        
         $jawab = get_inner_html($kolom->item(2));
         $arr_soal[count($arr_soal)]['pilihan'][$idx] = $jawab;
       }
