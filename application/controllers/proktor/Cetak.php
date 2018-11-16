9c0d1a8ccd2c5cb0c52a168811bf5e167ab89596
Akhmad Zaini <zaini1983@gmail.com>
2018-11-16 09:49:14 +0700

    Update Cetak.php
    Perbaikan cetak kartu peserta dari pak udi

--- application/controllers/proktor/Cetak.php
+++ application/controllers/proktor/Cetak.php
@@ -23,34 +23,77 @@ class Cetak extends Home_proktor{
     
     $spreadsheet = new Spreadsheet();
     $sheet = $spreadsheet->getActiveSheet();
-    $sheet->getColumnDimension('E')->setWidth(12);
+    $sheet->getColumnDimension('A')->setWidth(2);
+    $sheet->getColumnDimension('B')->setWidth(2);
+    $sheet->getColumnDimension('C')->setWidth(8);
+    $sheet->getColumnDimension('D')->setWidth(2);
+    $sheet->getColumnDimension('E')->setWidth(8);
+    $sheet->getColumnDimension('F')->setWidth(24);
+    $sheet->getColumnDimension('G')->setWidth(2);
+    $sheet->getColumnDimension('H')->setWidth(2);
+    $sheet->getColumnDimension('I')->setWidth(2);
+    $sheet->getColumnDimension('J')->setWidth(2);
+    $sheet->getColumnDimension('K')->setWidth(8);
+    $sheet->getColumnDimension('L')->setWidth(2);
+    $sheet->getColumnDimension('M')->setWidth(8);
+    $sheet->getColumnDimension('N')->setWidth(24);
     
     // Border untuk judul
-    $this->__garis_pinggir($sheet, 'A2:I8');
-    $this->__garis_pinggir($sheet, 'B3:C7');
+    //$this->__garis_pinggir($sheet, 'A2:F8');
+    //$this->__garis_pinggir($sheet, 'C3:C7');
+    //$this->__garis_pinggir($sheet, 'I2:M8');
+    //$this->__garis_pinggir($sheet, 'J3:J7');
     $sheet->setCellValue('E3', 'DINAS PENDIDIKAN PEMUDA DAN OLAHRAGA');
     $sheet->setCellValue('E4', 'KOTA PROBOLINGGO');
-    $sheet->setCellValue('E5', 'KARTU UJIAN ...');
+    $sheet->setCellValue('E5', 'KARTU UJIAN PESERTA');
     
-    $baris_awal = 9;
-    $baris_terakhir = 9 + 6;
+    $baris_awal = 2;
+    $baris_terakhir = 13;
     foreach($data as $r){
-      $this->__garis_pinggir($sheet, "A$baris_awal:I$baris_terakhir");
-      $this->__garis_pinggir($sheet, 'B' . ($baris_awal + 1) . ':C' . ($baris_awal + 5));
-      $sheet->setCellValue('E' . ($baris_awal + 1), 'USER');
-      $sheet->setCellValue('F' . ($baris_awal + 1), $r->login);
-      $sheet->setCellValue('E' . ($baris_awal + 2), 'PASSWORD');
-      $sheet->setCellValue('F' . ($baris_awal + 2), $r->password);
-      $sheet->setCellValue('E' . ($baris_awal + 3), 'SERVER');
-      $sheet->setCellValue('F' . ($baris_awal + 3), $r->server);
-      $sheet->setCellValue('E' . ($baris_awal + 4), 'NISN');
-      $sheet->setCellValue('F' . ($baris_awal + 4), $r->nis);
-      $sheet->setCellValue('E' . ($baris_awal + 5), 'NAMA');
-      $sheet->setCellValue('F' . ($baris_awal + 5), $r->nama);
+      $this->__garis_pinggir($sheet, "B$baris_awal".":"."G$baris_terakhir");
+	  $sheet->mergeCells('E'.($baris_awal + 1).':F'.($baris_awal + 1));	
+	  $sheet->mergeCells('E'.($baris_awal + 2).':F'.($baris_awal + 2));	
+	  $sheet->mergeCells('E'.($baris_awal + 3).':F'.($baris_awal + 3));		
+	  $sheet->setCellValue('E' . ($baris_awal + 1), 'DISDIPENDIK KOTA PROBOLINGGO');
+      $sheet->setCellValue('E' . ($baris_awal + 2), get_app_config('NAMA_SEKOLAH'));
+      $sheet->setCellValue('E' . ($baris_awal + 3), 'KARTU UJIAN PESERTA');
+      $this->__garis_pinggir($sheet, 'C' . ($baris_awal + 1) . ':C' . ($baris_awal + 3));
+      $this->__garis_pinggir($sheet, 'C' . ($baris_awal + 6) . ':C' . ($baris_awal + 10));
+      $sheet->setCellValue('E' . ($baris_awal + 6), 'USER');
+      $sheet->setCellValue('F' . ($baris_awal + 6), $r->login);
+      $sheet->setCellValue('E' . ($baris_awal + 7), 'PASS');
+      $sheet->setCellValue('F' . ($baris_awal + 7), $r->password);
+      $sheet->setCellValue('E' . ($baris_awal + 8), 'SERVER');
+      $sheet->setCellValue('F' . ($baris_awal + 8), $r->server);
+      $sheet->setCellValue('E' . ($baris_awal + 9), 'NISN');
+      $sheet->setCellValue('F' . ($baris_awal + 9), $r->nis);
+      $sheet->setCellValue('E' . ($baris_awal + 10), 'NAMA');
+      $sheet->setCellValue('F' . ($baris_awal + 10), $r->nama);
+	  
+	  
+      $this->__garis_pinggir($sheet, "J$baris_awal".":"."N$baris_terakhir");
+	  $sheet->mergeCells('M'.($baris_awal + 1).':N'.($baris_awal + 1));	
+	  $sheet->mergeCells('M'.($baris_awal + 2).':N'.($baris_awal + 2));	
+	  $sheet->mergeCells('M'.($baris_awal + 3).':N'.($baris_awal + 3));		
+	  $sheet->setCellValue('M' . ($baris_awal + 1), 'DISDIPENDIK KOTA PROBOLINGGO');
+      $sheet->setCellValue('M' . ($baris_awal + 2), get_app_config('NAMA_SEKOLAH'));
+      $sheet->setCellValue('M' . ($baris_awal + 3), 'KARTU MEJA');
+      $this->__garis_pinggir($sheet, 'K' . ($baris_awal + 1) . ':K' . ($baris_awal + 3));
+      $this->__garis_pinggir($sheet, 'K' . ($baris_awal + 6) . ':K' . ($baris_awal + 10));
+      $sheet->setCellValue('M' . ($baris_awal + 6), 'USER');
+      $sheet->setCellValue('N' . ($baris_awal + 6), $r->login);
+      $sheet->setCellValue('M' . ($baris_awal + 7), 'PASS');
+      $sheet->setCellValue('N' . ($baris_awal + 7), $r->password);
+      $sheet->setCellValue('M' . ($baris_awal + 8), 'SERVER');
+      $sheet->setCellValue('N' . ($baris_awal + 8), $r->server);
+      $sheet->setCellValue('M' . ($baris_awal + 9), 'NISN');
+      $sheet->setCellValue('N' . ($baris_awal + 9), $r->nis);
+      $sheet->setCellValue('M' . ($baris_awal + 10), 'NAMA');
+      $sheet->setCellValue('N' . ($baris_awal + 10), $r->nama);
       
-      $tambahan = ($baris_terakhir % 43 == 0) ? 6 : 1;
+      $tambahan = ($baris_terakhir % 43 == 0) ? 6 : 3;
       $baris_awal = $baris_terakhir + $tambahan;
-      $baris_terakhir = $baris_awal + 6;
+      $baris_terakhir = $baris_awal + 11;
     }
     
     $writer = new Xlsx($spreadsheet);
